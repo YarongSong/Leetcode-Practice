5474576228af6fb604386165fb34c4747d8e4d46
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-01-16 17:01:54 -0800

    Time: 1142 ms (41.60%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -2,12 +2,12 @@
 
 SELECT p.product_name, a.unit
 FROM(
-SELECT o.product_id, SUM(o.unit) AS unit
-FROM Orders o
-WHERE YEAR(o.order_date) = 2020
-AND MONTH(o.order_date) = 2
-GROUP BY o.product_id
+    SELECT o.product_id, SUM(o.unit) AS unit
+    FROM Orders o
+    WHERE YEAR(o.order_date) = 2020
+    AND MONTH(o.order_date) = 2
+    GROUP BY o.product_id
+    HAVING SUM(o.unit) >= 100
 ) a 
 INNER JOIN Products p 
 ON a.product_id = p.product_id
-WHERE a.unit >=100
