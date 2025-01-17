8cf1c656db6969a179e54a5c1ae626c94bc7d3ad
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-01-17 14:10:48 -0800

    Time: 495 ms (51.14%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+SELECT CASE WHEN MOD(id,2) = 0 THEN (id-1) 
+            WHEN id = (SELECT MAX(id) FROM Seat) THEN id
+            ELSE (id + 1) END AS id
+    , student
+FROM Seat
+ORDER BY 1
