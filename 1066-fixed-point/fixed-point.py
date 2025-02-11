938e09db64bee493fec9c5685af44a39a392e943
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-02-10 20:40:22 -0800

    Time: 0 ms (100.00%) | Memory: 18.6 MB (47.60%) - LeetSync

---
+++ 1066-fixed-point/fixed-point.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fixedPoint(self, arr: List[int]) -> int:
+        left = 0
+        right = len(arr) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if arr[mid] == mid:
+                right = mid
+            elif arr[mid] > mid:
+                right = mid - 1
+            elif arr[mid] < mid:
+                left = mid + 1
+        if arr[left] == left:
+            return left
+        elif arr[right] == right:
+            return right
+        return -1
+
+
+        
