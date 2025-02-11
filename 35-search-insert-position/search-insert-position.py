f6f101b9656af76f4c30f12fbaedc9f285785b4b
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-02-10 21:02:48 -0800

    Time: 0 ms (100.00%) | Memory: 18.5 MB (24.81%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,31 +1,71 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int: 
-        if len(nums) == 1 and nums[0] >= target:
-            return 0
-        elif len(nums) == 1 and nums[0] < target:
-            return 1
-
-
         left = 0
         right = len(nums) - 1
+
         while left < right-1:
-            mid = (left + right)//2
+            mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
                 left = mid
             else:
                 right = mid
-        if nums[left] == target:
+        
+        if nums[left] < target and nums[right] > target:
+            return left + 1
+        elif nums[left] == target:
             return left
+        elif nums[left] > target:
+            return max(left-1, 0)
         elif nums[right] == target:
             return right
-        elif nums[left] < target and nums[right] > target:
-            return left + 1
-        elif nums[left] > target:
-            return max(left - 1, 0)
-        else:
+        elif nums[right] < target:
             return right + 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if len(nums) == 1 and nums[0] >= target:
+        #     return 0
+        # elif len(nums) == 1 and nums[0] < target:
+        #     return 1
+
+
+        # left = 0
+        # right = len(nums) - 1
+        # while left < right-1:
+        #     mid = (left + right)//2
+        #     if nums[mid] == target:
+        #         return mid
+        #     elif nums[mid] < target:
+        #         left = mid
+        #     else:
+        #         right = mid
+        # if nums[left] == target:
+        #     return left
+        # elif nums[right] == target:
+        #     return right
+        # elif nums[left] < target and nums[right] > target:
+        #     return left + 1
+        # elif nums[left] > target:
+        #     return max(left - 1, 0)
+        # else:
+        #     return right + 1
 
 
 
