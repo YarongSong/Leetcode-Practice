9744e57d82d0b1fa260c2fdf8cfc310a98284c89
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-02-13 15:56:24 -0800

    Time: 23 ms (6.94%) | Memory: 19.4 MB (50.66%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         s = {}
-        res = []
         for i in range(len(nums)):
             if nums[i] in s:
                 s[nums[i]] += 1
@@ -9,8 +8,8 @@ class Solution:
                 s[nums[i]] = 1
 
             if s[nums[i]] > len(nums)/2:
-                res.append(nums[i])
+                return nums[i]
         
-        return res[0]
+        #return res[0]
 
         
