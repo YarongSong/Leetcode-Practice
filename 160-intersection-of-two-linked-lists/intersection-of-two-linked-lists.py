f1b1a9f45ff687f67e690cca9330dc51521b0993
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-03-12 17:07:03 -0700

    Time: 83 ms (69.02%) | Memory: 27.9 MB (43.05%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -4,17 +4,57 @@
 #         self.val = x
 #         self.next = None
 
+# ListNode can be save in hashset
+# node 1 and node are 2 nodes although their values and next node are the same
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        s = set()
-        while headA:
-            s.add(headA)
-            headA = headA.next
-        while headB:
-            if headB in s:
-                return headB
-            else:
+        l1 = 0
+        cur = headA
+        while cur:
+            l1 += 1
+            cur = cur.next
+        
+        l2 = 0
+        cur = headB
+        while cur:
+            l2 += 1
+            cur = cur.next
+
+        while headA and headB:
+            if l1 < l2:
                 headB = headB.next
+                l2 -= 1
+            elif l1 > l2:
+                headA = headA.next
+                l1 -= 1
+            else:
+                if headA == headB:
+                    return headA
+                else:
+                    headA = headA.next
+                    headB = headB.next
         return None
+
+
+
+
+
+
+
+
+
+
+
+
+        # s = set()
+        # while headA:
+        #     s.add(headA)
+        #     headA = headA.next
+        # while headB:
+        #     if headB in s:
+        #         return headB
+        #     else:
+        #         headB = headB.next
+        # return None
 
         
