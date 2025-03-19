2ee7a1d4dca6ef963fc552f0ebe9aae177a6b54d
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-03-18 17:27:52 -0700

    Time: 1696 ms (22.42%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1494-activity-participants/activity-participants.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+WITH cnt_each AS(
+    SELECT DISTINCT a.id, a.name, COUNT(DISTINCT f.id) AS CNT
+    FROM Activities a
+    LEFT JOIN Friends f
+    ON a.name = f.activity
+    GROUP BY 1,2
+), max_min AS(
+    SELECT MAX(CNT) AS max_cnt, MIN(CNT) AS min_cnt
+    FROM cnt_each
+)
+SELECT name AS activity
+FROM cnt_each
+WHERE CNT <> (SELECT max_cnt FROM max_min)
+AND CNT <> (SELECT min_cnt FROM max_min)
+;
+
