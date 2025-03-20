b20d07335dc9b6e4e53e51730ad4c51bfc46dfd5
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-03-20 16:31:10 -0700

    Time: 2142 ms (95.10%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+
+WITH CTE AS(
+SELECT c.caller_id, c.duration, ct.country_code, ct.name
+FROM (SELECT DISTINCT * FROM Calls) c
+LEFT JOIN Person p
+ON c.caller_id = p.id
+LEFT JOIN Country ct
+ON LEFT(p.phone_number,3) = ct.country_code
+
+UNION ALL
+
+SELECT c.callee_id, c.duration, ct.country_code, ct.name
+FROM (SELECT DISTINCT * FROM Calls) c
+LEFT JOIN Person p
+ON c.callee_id = p.id
+LEFT JOIN Country ct
+ON LEFT(p.phone_number,3) = ct.country_code
+)
+SELECT DISTINCT name AS country
+FROM CTE 
+GROUP BY country_code, name
+HAVING AVG(duration) > (SELECT AVG(duration) FROM CTE)
