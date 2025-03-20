91b707a75c44b18ec2aa44d33dafa203ee3679ce
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-03-20 16:06:50 -0700

    Time: 2839 ms (95.04%) | Memory: 0B (100.00%) - LeetSync

--- 1579-active-users/active-users.sql
+++ 1579-active-users/active-users.sql
@@ -3,9 +3,9 @@ SELECT DISTINCT A.id, B.name
 FROM(
     SELECT DISTINCT id
             , login_date
-            , LEAD(login_date, 1) OVER(PARTITION BY id ORDER BY login_date) AS login_date_2
-            , LEAD(login_date, 2) OVER(PARTITION BY id ORDER BY login_date) AS login_date_3
-            , LEAD(login_date, 3) OVER(PARTITION BY id ORDER BY login_date) AS login_date_4
+            -- , LEAD(login_date, 1) OVER(PARTITION BY id ORDER BY login_date) AS login_date_2
+            -- , LEAD(login_date, 2) OVER(PARTITION BY id ORDER BY login_date) AS login_date_3
+            -- , LEAD(login_date, 3) OVER(PARTITION BY id ORDER BY login_date) AS login_date_4
             , LEAD(login_date, 4) OVER(PARTITION BY id ORDER BY login_date) AS login_date_5
     FROM (SELECT DISTINCT id, login_date FROM Logins) a
     ) A
