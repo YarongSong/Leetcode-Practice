9e3df6bb89b897893ea22635cbd2e204fab263c5
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-03-23 11:57:34 -0700

    Time: 681 ms (19.62%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,25 @@
 # Write your MySQL query statement below
-SELECT MAX(salary) AS SecondHighestSalary
-FROM Employee e
-WHERE salary <> (SELECT MAX(salary) FROM Employee)
--- ORDER BY salary DESC
+-- SELECT MAX(salary) AS SecondHighestSalary
+-- FROM Employee e
+-- WHERE salary <> (SELECT MAX(salary) FROM Employee)
+-- ;
+
+SELECT IFNULL(
+    (
+    SELECT DISTINCT salary
+    FROM(
+        SELECT DISTINCT salary 
+            , DENSE_RANK() OVER(ORDER BY salary DESC) AS RNK
+        FROM Employee
+        ) A
+    WHERE RNK = 2
+    ), NULL) AS SecondHighestSalary
+;
+
+-- SELECT IFNULL(
+-- (SELECT DISTINCT Salary AS SecondHighestSalary
+-- FROM Employee
+-- ORDER BY Salary DESC
 -- LIMIT 1
+-- OFFSET 1), NULL) AS SecondHighestSalary
+-- ;
