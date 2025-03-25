221d49fc51e4edcd9e70df304aabc7428a5d8289
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-03-24 17:06:22 -0700

    Time: 581 ms (92.83%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+# person1 | person2 | call_count | total_duration
+
+SELECT CASE WHEN from_id < to_id THEN from_id ELSE to_id END AS person1
+    , CASE WHEN from_id < to_id THEN to_id ELSE from_id END AS person2
+    , COUNT(*) AS call_count
+    , SUM(duration) AS total_duration
+FROM Calls
+GROUP BY 1,2
+;
+
