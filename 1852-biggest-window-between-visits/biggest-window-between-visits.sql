861a1a0154f4152a0f554210930ed8f171a25faa
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-03-24 17:27:43 -0700

    Time: 484 ms (99.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1852-biggest-window-between-visits/biggest-window-between-visits.sql
@@ -0,0 +1,25 @@
+# Write your MySQL query statement below
+
+# user_id | biggest_window
+WITH cte AS(
+    SELECT user_id, visit_date
+    FROM UserVisits
+
+    UNION
+
+    SELECT user_id, DATE('2021-1-1') AS visit_date
+    FROM UserVisits
+
+)
+SELECT user_id 
+    , MAX(DATEDIFF(nxt_date, visit_date)) AS biggest_window
+FROM(
+    SELECT user_id
+        , visit_date
+        , LEAD(visit_date, 1) OVER(PARTITION BY user_id ORDER BY visit_date) AS nxt_date 
+    FROM cte
+) A
+GROUP BY 1
+ORDER BY 1
+;
+
