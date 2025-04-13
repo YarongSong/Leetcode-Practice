b004a7e0ed4c166c0d70fa9d87e4ed3ac2cc13e5
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-04-13 12:12:20 -0700

    Time: 2 ms (32.85%) | Memory: 17.9 MB (48.43%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,16 +1,60 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        m = len(strs)
-        l = len(strs[0])
-        for i in range(m):
-            if len(strs[i]) < l:
-                l = len(strs[i])
-        out = ''
-        for j in range(l):
-            for i in range(m):
+        l = len(strs)
+        min_l = len(strs[0])
+
+        # find the common length
+        for i in range(l):
+            if len(strs[i]) < min_l:
+                min_l = len(strs[i])
+
+        res = ""
+        for j in range(min_l):
+            for i in range(l):
                 if strs[i][j] != strs[0][j]:
-                    return out
-            out += strs[i][j]
-        return out
+                    return res
+            res += strs[0][j]
+        return res
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # m = len(strs)
+        # l = len(strs[0])
+        # for i in range(m):
+        #     if len(strs[i]) < l:
+        #         l = len(strs[i])
+        # out = ''
+        # for j in range(l):
+        #     for i in range(m):
+        #         if strs[i][j] != strs[0][j]:
+        #             return out
+        #     out += strs[i][j]
+        # return out
 
         
