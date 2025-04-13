27d62520f8b2ef146be6f3b332b7533327b403af
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-04-13 11:05:04 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (50.91%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,24 +1,19 @@
-# Only when the list is not empty, the index would work
-# lst.pop(): Remove and return the last item
-# lst.remove(x): Remove first occurrence of value x
 class Solution:
     def isValid(self, s: str) -> bool:
-        st = []
-        p = {")": "(", "}":"{", "]":"["}
-        o = {'(', '[', '{'}
-        for i in s:
-            if i in o:
-                st.append(i)
+        t = []
+        d = {')':'(', '}':'{', ']':'['}
+        e = {'(', '{', '['}
+        for i in range(len(s)):
+            if s[i] in e:
+                t.append(s[i])
+            elif len(t) != 0 and d[s[i]] == t[-1]:
+                t.pop()
             else:
-                if len(st) == 0:     ############
-                    return False
-                elif st[-1] == p[i]:
-                    st.pop()
-                else:
-                    return False
-                
-        if len(st) != 0:
+                return False
+
+        if len(t) == 0:
+            return True
+        else:
             return False
 
-        return True
         
