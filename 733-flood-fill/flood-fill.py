ca289407a552e400bc160c847eacb36635f8db57
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-04-19 20:18:51 -0700

    Time: 0 ms (100.00%) | Memory: 18.1 MB (50.43%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -5,22 +5,22 @@ class Solution:
 
         color_raw = image[sr][sc]
 
-        if color == color_raw:
-            return image
+        if color != color_raw:
+            # return image
        
-        image[sr][sc] = color
-        if sr-1 >= 0 and image[sr-1][sc] == color_raw:
-            self.floodFill(image, sr-1, sc, color)
-        
-        if sr+1 < m and image[sr+1][sc] == color_raw:
-            self.floodFill(image, sr+1, sc, color)
+            image[sr][sc] = color
+            if sr-1 >= 0 and image[sr-1][sc] == color_raw:
+                self.floodFill(image, sr-1, sc, color)
+            
+            if sr+1 < m and image[sr+1][sc] == color_raw:
+                self.floodFill(image, sr+1, sc, color)
 
-        if sc-1 >= 0 and image[sr][sc-1] == color_raw:
-            self.floodFill(image, sr, sc-1, color)
-        
-        if sc+1 < n and image[sr][sc+1] == color_raw:
-            self.floodFill(image, sr, sc+1, color)
-        
+            if sc-1 >= 0 and image[sr][sc-1] == color_raw:
+                self.floodFill(image, sr, sc-1, color)
+            
+            if sc+1 < n and image[sr][sc+1] == color_raw:
+                self.floodFill(image, sr, sc+1, color)
+            
         return image
         
 
