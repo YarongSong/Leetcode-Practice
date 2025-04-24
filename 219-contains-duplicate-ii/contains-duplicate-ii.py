37dfa0c70a31aa6101cb12c6367f9843ef69ece8
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-04-23 17:43:17 -0700

    Time: 27 ms (53.19%) | Memory: 36.7 MB (12.29%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -6,17 +6,22 @@ class Solution:
         d = {}
         for i in range(len(nums)):
             if nums[i] in d:
-                d[nums[i]].append(i)
-            else:
-                d[nums[i]] = [i]
-        
-        for key, value in d.items():
-            i = 0
-            j = 1
-            while i <= len(value) - 2:
-                if value[j] - value[i] <= k:
+                if i - d[nums[i]] <= k:
                     return True
-                i += 1
-                j += 1
+                else:
+                    d[nums[i]] = i
+            else:
+                d[nums[i]] = i
         
         return False
+        
+        # for key, value in d.items():
+        #     i = 0
+        #     j = 1
+        #     while i <= len(value) - 2:
+        #         if value[j] - value[i] <= k:
+        #             return True
+        #         i += 1
+        #         j += 1
+        
+        # return False
