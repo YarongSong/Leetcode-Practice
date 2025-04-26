e7c2291c446722607b60eab631011d958d206d10
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-04-26 10:53:48 -0700

    Time: 6 ms (35.85%) | Memory: 20.2 MB (7.30%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,18 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
+        return sum(set(nums)) * 2 - sum(nums)
+        # if len(nums) == 1:
+        #     return nums[0]
 
-        dic = {}
-        for n in nums:
-            if n in dic:
-                dic[n] += 1
-            else:
-                dic[n] = 1
+        # dic = {}
+        # for n in nums:
+        #     if n in dic:
+        #         dic[n] += 1
+        #     else:
+        #         dic[n] = 1
 
-        for key, value in dic.items():
-            if value == 1:
-                return key
+        # for key, value in dic.items():
+        #     if value == 1:
+        #         return key
 
 
 
