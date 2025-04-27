94b79a82cd9494e83dd902955cc8c503fa9ab44a
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-04-27 12:01:12 -0700

    Time: 23 ms (94.88%) | Memory: 23.2 MB (44.35%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.py
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -7,14 +7,9 @@ class RecentCounter:
     def ping(self, t: int) -> int:
         self.res.append(t)
         i = 0
-        while i < len(self.res):
-            if self.res[i] < t - 3000:
-                self.res.popleft()
-                i = 0
-            else:
-                break
-
-        
+        while self.res[0] < t - 3000:
+            self.res.popleft()
+       
         return len(self.res)
         
 
