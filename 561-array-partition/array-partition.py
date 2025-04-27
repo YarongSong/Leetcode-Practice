5e2ef9067237415c3f9790d8c221066e60adf8a6
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-04-27 10:10:57 -0700

    Time: 62 ms (5.14%) | Memory: 19.6 MB (68.60%) - LeetSync

--- 561-array-partition/array-partition.py
+++ 561-array-partition/array-partition.py
@@ -1,11 +1,13 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
+
         nums.sort()
+        print(nums)
         res = 0
         for i in range(len(nums)):
             if i % 2 == 0:
+                print(nums[i])
                 res += nums[i]
-        
         return res
 
         
