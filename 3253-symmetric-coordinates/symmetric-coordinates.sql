1ce39151e99d79ba150cf0833a5d26b14f9b0e8a
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-05-07 16:03:07 -0700

    Time: 303 ms (56.76%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3253-symmetric-coordinates/symmetric-coordinates.sql
@@ -0,0 +1,30 @@
+# Write your MySQL query statement below
+
+-- x  | y 
+-- Order: x, y, ASC
+
+WITH two_sce AS(
+    SELECT a.X AS x
+        , a.Y AS y
+    FROM (SELECT DISTINCT X, Y FROM Coordinates WHERE X <> Y) a
+    JOIN (SELECT DISTINCT X, Y FROM Coordinates WHERE X <> Y) b
+    ON a.X = b.Y
+    AND a.Y = b.X
+
+    UNION
+
+    SELECT X AS x
+        , Y AS y
+    FROM Coordinates
+    WHERE X = Y
+    GROUP BY 1,2
+    HAVING COUNT(*) >= 2
+)
+
+
+SELECT x, y
+FROM two_sce
+WHERE x <= y
+ORDER BY x, y
+;
+
