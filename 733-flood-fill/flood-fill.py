54a5a40adb5bd67a4515dd119a683b820deda18c
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-05-18 15:10:12 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (88.37%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,7 +1,5 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        if sr < 0 or sr > len(image) or sc < 0 or sc > len(image[0]):
-            return
         
         if image[sr][sc] == color:
             return image
@@ -9,17 +7,18 @@ class Solution:
         raw_color = image[sr][sc]
         image[sr][sc] = color
         
-        if sr - 1 >= 0 and image[sr - 1][sc] == raw_color:
-            self.floodFill(image,sr-1,sc,color)
-        if sr + 1 < len(image) and image[sr + 1][sc] == raw_color:
-            self.floodFill(image,sr+1,sc,color)
-        if sc - 1 >= 0 and image[sr][sc-1] == raw_color:
-            self.floodFill(image,sr,sc-1,color)
-        if sc +1 < len(image[0]) and image[sr][sc+1] == raw_color:
-            self.floodFill(image,sr,sc+1,color)
+        if sr-1 >=0 and image[sr-1][sc] == raw_color:
+            self.floodFill(image, sr-1, sc, color)
+        if sr+1 <= len(image) - 1 and image[sr+1][sc] == raw_color:
+            self.floodFill(image, sr+1, sc, color)
+        if sc-1 >=0 and image[sr][sc-1] == raw_color:
+            self.floodFill(image, sr, sc-1, color)
+        if sc+1 <= len(image[0]) - 1 and image[sr][sc+1] == raw_color:
+            self.floodFill(image, sr, sc+1, color)
         
         return image
 
-               
+
+        
 
         
