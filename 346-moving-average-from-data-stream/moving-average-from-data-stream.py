cd0c1327f716f58ed7881f88c87fdbb8a0b08585
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-05-18 16:11:08 -0700

    Time: 23 ms (28.92%) | Memory: 21 MB (58.81%) - LeetSync

--- 346-moving-average-from-data-stream/moving-average-from-data-stream.py
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.py
@@ -1,18 +1,18 @@
 class MovingAverage:
 
     def __init__(self, size: int):
-        self.sz = size
+        self.size = size
         self.dq = deque()
+        
 
     def next(self, val: int) -> float:
-        if len(self.dq) < self.sz:
+        if len(self.dq) < self.size:
             self.dq.append(val)
         else:
             self.dq.popleft()
             self.dq.append(val)
         
         return sum(self.dq)/len(self.dq)
-
 
         
 
