bf702794d6d314f6d9a864310f024f5b4066cd35
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-05-19 18:53:01 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (9.46%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,29 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        res = deque()
+        i = len(digits) - 1
+        while i >= 0:
+            if (i == len(digits) - 1) and (digits[i] + carry != 10):
+                res.appendleft(digits[i] + carry)
+                carry = 0
+            elif i == len(digits) - 1:
+                res.appendleft(0)
+                carry = 1
+            elif digits[i] + carry != 10:
+                res.appendleft(digits[i] + carry)
+                carry = 0
+            else:
+                res.appendleft(0)
+                carry = 1
+            
+            i -= 1
+       
+        if carry == 1:
+            res.appendleft(1)
+        
+        return list(res)
+
+
+
+        
