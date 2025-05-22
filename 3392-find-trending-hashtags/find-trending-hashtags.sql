71ce4e11da256add97aae6de0d9691500e99cd30
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-05-22 10:37:49 -0700

    Time: 286 ms (72.73%) | Memory: 0B (100.00%) - LeetSync

--- 3392-find-trending-hashtags/find-trending-hashtags.sql
+++ 3392-find-trending-hashtags/find-trending-hashtags.sql
@@ -10,17 +10,46 @@
 
 SELECT hashtag, hashtag_count
 FROM(
-    SELECT hashtag, hashtag_count, RANK() OVER(ORDER BY hashtag_count DESC, hashtag DESC) AS RNK
+    SELECT *, RANK() OVER(ORDER BY hashtag_count DESC, hashtag DESC) AS RNK
     FROM(
         SELECT REGEXP_SUBSTR(tweet, '\#[a-zA-Z0-9]+') AS hashtag, COUNT(tweet_id) AS hashtag_count
         FROM Tweets
         WHERE DATE_FORMAT(tweet_date, '%Y-%m') = '2024-02'
         GROUP BY 1
-        ) A
-    ) B
+    ) A
+) B
 WHERE RNK <= 3
 ORDER BY 2 DESC, 1 DESC
 ;
+
+
+
+
+
+
+
+
+
+
+
+
+-- SELECT hashtag, hashtag_count
+-- FROM(
+--     SELECT hashtag, hashtag_count, RANK() OVER(ORDER BY hashtag_count DESC, hashtag DESC) AS RNK
+--     FROM(
+--         SELECT REGEXP_SUBSTR(tweet, '\#[a-zA-Z0-9]+') AS hashtag, COUNT(tweet_id) AS hashtag_count
+--         FROM Tweets
+--         WHERE DATE_FORMAT(tweet_date, '%Y-%m') = '2024-02'
+--         GROUP BY 1
+--         ) A
+--     ) B
+-- WHERE RNK <= 3
+-- ORDER BY 2 DESC, 1 DESC
+-- ;
+
+
+
+
 
 
 
