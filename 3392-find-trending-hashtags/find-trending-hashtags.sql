80e473ef00d806d5b63dcbdeda36af121f6a39ef
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-05-25 10:34:49 -0700

    Time: 289 ms (65.00%) | Memory: 0B (100.00%) - LeetSync

--- 3392-find-trending-hashtags/find-trending-hashtags.sql
+++ 3392-find-trending-hashtags/find-trending-hashtags.sql
@@ -1,55 +1,24 @@
 # Write your MySQL query statement below
 
--- hashtag  | hashtag_count
--- Order: hashtag_count DESC, hashtag DESC
+-- hashtag | hashtag_count
+-- 1) find the hashtag for each tweet
+-- 2) group by tweet, count
+-- 3) rank top3
+-- 4) order: count of hashtag DESC, hashtag DESC
 
--- 1) 2024, 2
--- 2) Find the hashtag for each tweet
--- 3) aggregate
--- 4) RANK
-
-SELECT hashtag, hashtag_count
+SELECT hashtag
+    , hashtag_count
 FROM(
-    SELECT *, RANK() OVER(ORDER BY hashtag_count DESC, hashtag DESC) AS RNK
+    SELECT hashtag
+        , hashtag_count
+        , RANK() OVER(ORDER BY hashtag_count DESC, hashtag DESC) AS RNK
     FROM(
-        SELECT REGEXP_SUBSTR(tweet, '\#[a-zA-Z0-9]+') AS hashtag, COUNT(tweet_id) AS hashtag_count
+        SELECT REGEXP_SUBSTR(tweet, '\#[a-zA-Z0-9]+') AS hashtag
+            , COUNT(tweet_id) AS hashtag_count
         FROM Tweets
-        WHERE DATE_FORMAT(tweet_date, '%Y-%m') = '2024-02'
         GROUP BY 1
-    ) A
-) B
+        ) A
+    ) B
 WHERE RNK <= 3
-ORDER BY 2 DESC, 1 DESC
+ORDER BY hashtag_count DESC, hashtag DESC
 ;
-
-
-
-
-
-
-
-
-
-
-
-
--- SELECT hashtag, hashtag_count
--- FROM(
---     SELECT hashtag, hashtag_count, RANK() OVER(ORDER BY hashtag_count DESC, hashtag DESC) AS RNK
---     FROM(
---         SELECT REGEXP_SUBSTR(tweet, '\#[a-zA-Z0-9]+') AS hashtag, COUNT(tweet_id) AS hashtag_count
---         FROM Tweets
---         WHERE DATE_FORMAT(tweet_date, '%Y-%m') = '2024-02'
---         GROUP BY 1
---         ) A
---     ) B
--- WHERE RNK <= 3
--- ORDER BY 2 DESC, 1 DESC
--- ;
-
-
-
-
-
-
-
