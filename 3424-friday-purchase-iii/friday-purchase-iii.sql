901fc1f85b9337d1d51de71f7c7dc3cede547e38
Yarong <103093928+YarongSong@users.noreply.github.com>
2025-05-25 14:44:45 -0700

    Time: 693 ms (68.13%) | Memory: 0B (100.00%) - LeetSync

--- 3424-friday-purchase-iii/friday-purchase-iii.sql
+++ 3424-friday-purchase-iii/friday-purchase-iii.sql
@@ -1,50 +1,52 @@
 # Write your MySQL query statement below
 
 -- week_of_month | membership  | total_amount
--- Order: week_of_month, membership ASC
 
--- convert the purchase_date to weekday (4)
--- All premium and VIP users, each Friday: total spend
--- no purchases at all
+-- 1) Each friday; Premium, VIP
+-- 2) friday, week of month
+-- 3) Premium, VIP
+-- 4) join Purchases & Users
+-- 5) aggregation
+-- 6) order: week of the month ASC, membership ASC
 
-WITH RECURSIVE dates AS(
-    -- Anchor query
-    SELECT DATE('2023-11-01') AS Date_Nov
+WITH RECURSIVE days AS(
+    SELECT DATE('2023-11-01') AS EACH_DAY
 
     UNION
 
-    -- Recursive query
-    SELECT DATE_ADD(Date_Nov, INTERVAL 1 DAY) AS Date_Nov
-    FROM dates
-    WHERE Date_Nov <= DATE('2023-11-29')
+    SELECT DATE_ADD(EACH_DAY, INTERVAL 1 DAY) AS EACH_DAY
+    FROM days
+    WHERE EACH_DAY <= DATE('2023-11-29')
 ), friday AS(
-    SELECT Date_Nov 
-    FROM dates
-    WHERE WEEKDAY(Date_Nov) = 4
-), member AS(
-
+    SELECT EACH_DAY, week_of_month
+    FROM(
+        SELECT EACH_DAY
+            , FLOOR((EACH_DAY - DATE('2023-11-01'))/7) + 1 AS week_of_month
+        FROM days
+    ) A
+    WHERE DAYOFWEEK(EACH_DAY) = 6
+), mem AS(
     SELECT 'Premium' AS membership
-    UNION 
+    UNION
     SELECT 'VIP' AS membership
+), purchase AS(
+    SELECT a.user_id, a.purchase_date, a.amount_spend, b.membership
+    FROM Purchases a
+    JOIN Users b
+    ON a.user_id = b.user_id
+    WHERE DAYOFWEEK(a.purchase_date) = 6
+    AND b.membership IN ('Premium', 'VIP')
 )
 
-SELECT FLOOR(DATEDIFF(a.Date_Nov , DATE('2023-11-01'))/7) + 1 AS week_of_month
-    , d.membership
-    , SUM(IFNULL(b.amount_spend,0)) AS total_amount
+SELECT a.week_of_month
+    , b.membership
+    , SUM(IFNULL(c.amount_spend,0)) AS total_amount
 FROM friday a
-CROSS JOIN member d
-LEFT JOIN (
-    SELECT A.*, B.membership 
-    FROM Purchases A
-    JOIN Users B
-    ON A.user_id = B.user_id
-    WHERE membership IN ('Premium', 'VIP')
-    ) b
-ON b.purchase_date = a.Date_Nov
-AND d.membership = b.membership
--- LEFT JOIN (SELECT * FROM Users WHERE membership IN ('Premium', 'VIP')) c
--- ON b.user_id  = c.user_id
--- AND c.membership = d.membership
-GROUP BY 1, 2
+CROSS JOIN mem b
+LEFT JOIN purchase c
+ON a.EACH_DAY = c.purchase_date
+AND b.membership = c.membership
+GROUP BY 1,2
 ORDER BY 1,2
 ;
+
